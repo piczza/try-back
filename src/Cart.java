84c90bd8b49e8423a1f50920da5678719698697a
Yeji Kim <kimpizzzaa@gmail.com>
2023-12-30 04:29:11 +0900

    구현: 장바구나 상품 추가/제거/출력

---
+++ src/Cart.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private Map<Product, Integer> cart = new HashMap<>();
+
+    //추가=============================
+    public void addProduct(Product product, int amount){
+        cart.merge(product, amount, Integer::sum); //캬! 머지!
+    }
+
+    //삭제=============================
+    public void removeProduct(Product product, int amount){
+        if(cart.containsKey(product)) cart.replace(product,cart.get(product)-amount);
+        if(cart.get(product) <= 0) cart.remove(product);
+    }
+
+    //화면에 장바구니 출력================
+    public void showItems(){
+        System.out.println("내 카트 출력======================");
+        for (Map.Entry<Product, Integer> entry : cart.entrySet()){
+            System.out.printf("%s가 %d개", entry.getKey().getName(), entry.getValue());
+        }
+    }
+
+
+
+}
--- src/CartApp.java
+++ src/CartApp.java
@@ -1,23 +1,30 @@
 import java.util.HashSet;
 import java.util.Set;
-
+import java.io.*;
 
 public class CartApp{
     public static void main(String[] args) {
+        //상품 목록 생성
+        Set<Product> productSet = new HashSet<>();
         Product book = new Product("책");
         Product mouse = new Product("마우스");
         Product phone = new Product("폰");
-
-        //상품 목록 생성
-        Set<Product> productSet = new HashSet<>();
         productSet.add(book);
         productSet.add(mouse);
         productSet.add(phone);
 
         //상품 목록 확인
-        System.out.println("고유한 상품 목록: ");
+        System.out.println("고유한 상품 목록======================");
         for(Product product : productSet){
-            System.out.println(product.getName() + " : " + product.getPrice());
+            System.out.println(product.getName() + ": " + product.getPrice() + "원");
         }
+
+        //장바구니
+        Cart myCart = new Cart();
+
+        //장바구니에 상호작용
+        myCart.addProduct(book, 5);
+
+        myCart.showItems();
     }
 }
