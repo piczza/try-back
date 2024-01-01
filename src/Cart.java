cbac5ebef0124096b2637eb796448ddb39662c67
Yeji Kim <kimpizzzaa@gmail.com>
2024-01-01 21:24:55 +0900

    구현: 콘솔창 인터페이스 내 상품 추가/제거/출력

--- src/Cart.java
+++ src/Cart.java
@@ -11,18 +11,23 @@ public class Cart {
 
     //삭제=============================
     public void removeProduct(Product product, int amount){
-        if(cart.containsKey(product)) cart.replace(product,cart.get(product)-amount);
+        if(cart.containsKey(product)) cart.put(product,cart.get(product)-amount);
+        else {
+            System.out.println("이미 존재하지 않는 상품입니다");
+            cart.put(product,-1);
+        }
         if(cart.get(product) <= 0) cart.remove(product);
+
     }
 
     //화면에 장바구니 출력================
     public void showItems(){
-        System.out.println("내 카트 출력=========================");
+        System.out.println("내 장바구니 출력==================================");
         for (Map.Entry<Product, Integer> entry : cart.entrySet()){
-            System.out.printf("%s가 %d개", entry.getKey().getName(), entry.getValue());
+            System.out.printf("%s: %d개\n", entry.getKey().getName(), entry.getValue());
         }
+        System.out.println("==============================================");
     }
-
 
 
 }
